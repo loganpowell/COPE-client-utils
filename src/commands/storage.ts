87d54e5acabafe4af6f7cdf618ec957727b329c7
Logan Powell <loganp@tepper.cmu.edu>
2021-07-22 13:02:07 -0400

    fixes lint

--- src/commands/storage.ts
+++ src/commands/storage.ts
@@ -10,92 +10,6 @@ import * as api from "../graphql/API"
 import { $global$ } from "./amplifyConfig"
 import { CRUD, MIMETypes } from "../utils"
 
-/*
-import React, { useEffect, useState } from 'react';
-import { Storage, API, graphqlOperation } from 'aws-amplify'
-import { v4 as uuid } from 'uuid'
-import { withAuthenticator } from '@aws-amplify/ui-react'
-
-import { createProduct as CreateProduct } from './graphql/mutations'
-import { listProducts as ListProducts } from './graphql/queries'
-import config from './aws-exports'
-
-const {
-  aws_user_files_s3_bucket_region: region,
-  aws_user_files_s3_bucket: bucket
-} = config
-
-function App() {
-  const [file, updateFile] = useState(null)
-  const [productName, updateProductName] = useState('')
-  const [products, updateProducts] = useState([])
-  useEffect(() => {
-    listProducts()
-  }, [])
-
-  // Query the API and save them to the state
-  async function listProducts() {
-    const products = await API.graphql(graphqlOperation(ListProducts))
-    updateProducts(products.data.listProducts.items)
-  }
-
-  function handleChange(event) {
-    const { target: { value, files } } = event
-    const fileForUpload = files[0]
-    updateProductName(fileForUpload.name.split(".")[0])
-    updateFile(fileForUpload || value)
-  }
-
-  // upload the image to S3 and then save it in the GraphQL API
-  async function createProduct() {
-    if (file) {
-      const extension = file.name.split(".")[1]
-      const { type: mimeType } = file
-      const key = `images/${uuid()}${productName}.${extension}`      
-      const url = `https://${bucket}.s3.${region}.amazonaws.com/public/${key}`
-      const inputData = { name: productName , image: url }
-
-      try {
-        await Storage.put(key, file, {
-          contentType: mimeType
-        })
-        await API.graphql(graphqlOperation(CreateProduct, { input: inputData }))
-      } catch (err) {
-        console.log('error: ', err)
-      }
-    }
-  }
-
-  return (
-    <div style={styles.container}>
-      <input
-        type="file"
-        onChange={handleChange}
-        style={{margin: '10px 0px'}}
-      />
-      <input
-        placeholder='Product Name'
-        value={productName}
-        onChange={e => updateProductName(e.target.value)}
-      />
-      <button
-        style={styles.button}
-        onClick={createProduct}>Create Product</button>
-
-      {
-        products.map((p, i) => (
-          <img
-            style={styles.image}
-            key={i}
-            src={p.image}
-          />
-        ))
-      }
-    </div>
-  );
-}
-*/
-
 enum Level {
     private = "private",
     public = "public",
@@ -136,7 +50,7 @@ export const storeObject = async (
         editors,
     }: CreateFileAssetInput,
     isAssetPr = true,
-    level: Level = Level.protected,
+    level = Level.protected,
 ) => {
     const {
         // @ts-ignore
@@ -152,9 +66,9 @@ export const storeObject = async (
     const extension = file_parts[cut]
     const UID = uuid()
     const key = `${extension}/${UID}--${file_name}`
-    console.log({ key, fileForUpload, level, mimeType, file_name })
-    const { id: user_id, ...rest } = await Auth.currentUserInfo()
-    console.log({ user_id, ...rest })
+    //console.log({ key, fileForUpload, level, mimeType, file_name })
+    const { id: user_id } = await Auth.currentUserInfo()
+    //console.log({ user_id, ...rest })
     return await Storage.put(key, fileForUpload, {
         level,
         contentType: mimeType,
@@ -162,7 +76,7 @@ export const storeObject = async (
         //progressCallback
     })
         .then(async s3Response => {
-            console.log({ s3Response })
+            //console.log({ s3Response })
             const { data } = await CRUD({
                 query: (isAssetPr && mutations.createAssetPr) || mutations.createAsset,
                 variables: {
@@ -180,7 +94,7 @@ export const storeObject = async (
                     },
                 },
             })
-            console.log({ data })
+            //console.log({ data })
             return (isAssetPr && data.createAssetPr) || data.createAsset
         })
         .catch(error => {
