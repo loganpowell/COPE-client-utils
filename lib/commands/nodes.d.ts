bf6ebff090bce3fd12a757658659fbb9a580fd66
Logan Powell <loganp@tepper.cmu.edu>
2021-08-11 16:00:37 -0400

    return val for nodesByOwnerType

--- lib/commands/nodes.d.ts
+++ lib/commands/nodes.d.ts
@@ -9,7 +9,7 @@ export declare const getConnectedNodesByNodeID: ({ id, edgeType }: GetNodeOption
 export declare const node: {
     create: ({ id, status, type, createdAt, owner, updatedAt }: api.CreateNodeInput, authMode?: GRAPHQL_AUTH_MODE) => Promise<api.Node>;
     read: ({ id }: api.GetNodeQueryVariables, authMode?: GRAPHQL_AUTH_MODE) => Promise<api.Node>;
-    update: ({ id, type, status, owner, createdAt }: api.UpdateNodeInput, authMode?: GRAPHQL_AUTH_MODE) => Promise<api.Node>;
+    update: ({ id, type, status, owner, createdAt }: api.UpdateNodeInput, authMode?: GRAPHQL_AUTH_MODE, titleize?: boolean) => Promise<api.Node>;
     delete: ({ id }: api.DeleteNodeInput, authMode?: GRAPHQL_AUTH_MODE) => Promise<api.Node>;
     connections: ({ id, edgeType }: GetNodeOptionsQueryVariables, authMode?: GRAPHQL_AUTH_MODE) => Promise<Array<api.Edge>>;
     list: ({ filter, limit, nextToken, sortDirection, owner, status, createdAt, type, }: ListNodesInput, authMode?: GRAPHQL_AUTH_MODE) => Promise<Array<api.Node>>;
